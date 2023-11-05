01769f2afb308853b1960afdd1cd3da983bd8f11
Paulo Schneider <paulo.schneider@gmail.com>
2023-11-05 15:03:56 +0000

    Increase score when goal scored

--- scenes/game/board/walls/vertical_wall.gd
+++ scenes/game/board/walls/vertical_wall.gd
@@ -8,5 +8,5 @@ signal goal_scored(ball)
 @export var direction: Vector2
 
 
-func ball_hit(ball: Ball, collision: KinematicCollision2D):
+func ball_hit(ball: Ball, _collision: KinematicCollision2D):
 	goal_scored.emit(ball)
--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
 
+var left_player_score: int = 0
+var right_player_score: int = 0
+
 
 func _on_goal_scored(goal: Goal, ball: Ball):
+	if goal.direction == Vector2.LEFT:
+		right_player_score += 1
+	else:
+		left_player_score += 1
+		
 	ball.reset(goal.direction)
