af9f4dfdd7c87933dce575cfaff42d6f2748bb73
Paulo Schneider <paulo.schneider@gmail.com>
2023-11-18 15:20:07 +0000

    Fix bug where fast subsequent hits would mess up color after glow

--- src/game/sprites/glow_sprite.gd
+++ src/game/sprites/glow_sprite.gd
@@ -2,7 +2,10 @@ extends Sprite2D
 class_name GlowSprite
 
 
+@onready var initial_color: Color = modulate
+
+
 func glow(color: Color):
 	var tween = create_tween()
 	tween.tween_property(self, "modulate", color, 0.3).set_trans(Tween.TRANS_QUAD)
-	tween.tween_property(self, "modulate", modulate, 0.2)
+	tween.tween_property(self, "modulate", initial_color, 0.2)
