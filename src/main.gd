78288ec880754fb3c791b7216db08b678ca9ccf6
Paulo Schneider <paulo.schneider@gmail.com>
2023-11-19 21:49:35 +0000

    Start game on enter

--- src/main.gd
+++ src/main.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 
+var title: PackedScene = preload("res://src/title/title.tscn")
 var game: PackedScene = preload("res://src/game/game.tscn")
 
 
 func _ready():
+	get_tree().change_scene_to_packed(title)	
+
+
+func start_game():
 	get_tree().change_scene_to_packed(game)
